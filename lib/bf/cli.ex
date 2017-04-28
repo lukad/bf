0171d09c11562f97d4f2acc920af33a7934c0825
Luka Dornhecker <luka.dornhecker@gmail.com>
2017-04-28 23:42:26 +0200

    add subcommand to print a program's ast

--- lib/bf/cli.ex
+++ lib/bf/cli.ex
@@ -4,8 +4,15 @@ defmodule Bf.CLI do
   def main(args) do
     optimus_parser()
     |> Optimus.parse!(args)
-    |> read_program
-    |> run_program
+    |> run
+  end
+
+  defp run(%{args: %{program_file: program_file}}) do
+    run_program(program_file)
+  end
+
+  defp run({[:ast], %{args: %{program_file: program_file}}}) do
+    print_ast(program_file)
   end
 
   defp optimus_parser do
@@ -16,25 +23,55 @@ defmodule Bf.CLI do
       args: [
         program_file: [
           value_name: "PROGRAM_FILE",
-          help: "Brainfuck program file to execute",
+          help: "The brainfuck program file to execute",
           required: true,
+        ]
+      ],
+      subcommands: [
+        ast: [
+          name: "ast",
+          about: "Prints the AST of the parsed program",
+          args: [
+            program_file: [
+              value_name: "PROGRAM_FILE",
+              help: "The brainfuck program file to parse",
+              required: true
+            ]
+          ]
         ]
       ]
     )
   end
 
-  defp read_program(%{args: %{program_file: program_file}}) do
+  defp read_program(program_file) do
     program_file
     |> File.read
+    |> case do
+         {:ok, program} -> program
+         {:error, reason} ->
+           IO.puts("Could not read prgram: #{reason}")
+           System.halt(1)
+       end
   end
 
-  defp run_program({:ok, program}) do
-    Bf.run(program)
+  defp run_program(program_file) do
+    program_file
+    |> read_program
+    |> Bf.run
   end
 
-  defp run_program({:error, reason}) do
-    IO.puts("Could not read program: #{reason}")
-    System.halt(1)
+  defp print_ast(program_file) do
+    program_file
+    |> read_program
+    |> to_char_list
+    |> parse_program
+    |> IO.inspect(limit: :infinity)
+  end
+
+  defp parse_program(program) do
+    {:ok, tokens, _} = :lexer.string(program)
+    {:ok, ast} = :parser.parse(tokens)
+    ast
   end
 
   defp version do
