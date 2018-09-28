409e7ee0c88233403b6fc5f6d24eca8adcc7e863
Luka Dornhecker <luka.dornhecker@gmail.com>
2018-09-28 22:15:46 +0200

    bump version to 1.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bf.Mixfile do
   def project do
     [
       app: :bf,
-      version: "1.2.0",
+      version: "1.3.0",
       elixir: "~> 1.3",
       description: description(),
       package: package(),
